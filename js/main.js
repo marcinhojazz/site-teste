c00b1857ab6e479ee5cb3807628ee4de4f49e28e
Marcio Machado Pontes <marcinhojazz@gmail.com>
2020-03-19 10:15:32 -0300

    Start project - Projeto de estudos em andamento - Aula 07

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 2000,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+});
